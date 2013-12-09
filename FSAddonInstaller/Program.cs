f888afb683c67fd9977fd4a7d3c7a13c7ad6a8cb
Steven Frost <steve.frost.private@gmail.com>
2013-12-09 23:03:25 +0000

    Introduced object-orientation

--- FSAddonInstaller/Program.cs
+++ FSAddonInstaller/Program.cs
@@ -9,28 +9,29 @@ namespace FSAddonInstaller {
         /// <summary>
         /// Program states
         /// </summary>
-        enum State {
+        private enum State {
             INSTALL,
             UNINSTALL
         }
 
         /* String constants */
-        private static string WELCOME_MESSAGE = "Generic FSX Addon Installer/Uninstaller\r\nBy Orion Lyau\r\n\r\nUsage:\r\n[directory]\t- Installs files from the specified directory into FSX.\r\n\t\t- Generates an XML file for uninstallation.\r\n[*.xml]\t\t- Triggers uninstall based on XML configuration file.\r\n\r\n";
-        private static string EXIT_MESSAGE = "\r\nPress any key to close...";
-        private static string INVALID_DIR_OR_FILE = "ERROR: An invalid directory or file was passed as an argument.";
-        private static string INVALID_NUM_ARGS = "ERROR: Invalid number of arguments.";
-        private static string XML_OUTPUT = "\r\n\r\n\tSource directory: {0}\r\n\tDestination directory: {1}\r\n\tTime stamp: {2}\r\n\tProcessed: {3} files, {4} directories\r\n\tSkipped {5} file(s).\r\n\tBacked up {6} file(s).\r\n\r\n";
-        private static string FSX_REG = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\microsoft games\flight simulator\10.0";
+        private const string WELCOME_MESSAGE = "Generic FSX Addon Installer/Uninstaller\r\nBy Orion Lyau\r\n\r\nUsage:\r\n[directory]\t- Installs files from the specified directory into FSX.\r\n\t\t- Generates an XML file for uninstallation.\r\n[*.xml]\t\t- Triggers uninstall based on XML configuration file.\r\n\r\n";
+        private const string EXIT_MESSAGE = "\r\nPress any key to close...";
+        private const string INVALID_DIR_OR_FILE = "ERROR: An invalid directory or file was passed as an argument.";
+        private const string INVALID_NUM_ARGS = "ERROR: Invalid number of arguments.";
+        private const string XML_OUTPUT = "\r\n\r\n\tSource directory: {0}\r\n\tDestination directory: {1}\r\n\tTime stamp: {2}\r\n\tProcessed: {3} files, {4} directories\r\n\tSkipped {5} file(s).\r\n\tBacked up {6} file(s).\r\n\r\n";
+        private const string FSX_REG = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\microsoft games\flight simulator\10.0";
 
         /* Program members */
-        private static State state;
-        private static string fsxDirectory = null;
-        private static int directoryCount = 0;
-        private static int fileCount = 0;
-        private static int skipCount = 0;
-        private static int backupCount = 0;
-        private static XDocument xmlConfig;
-        private static bool backupAndReplaceEnabled = true;
+        private State state;
+        private string path;
+        private string fsxDirectory;
+        private int directoryCount;
+        private int fileCount;
+        private int skipCount;
+        private int backupCount;
+        private XDocument xmlConfig;
+        private bool backupAndReplaceEnabled;
 
         /// <summary>
         /// Main program entry point
@@ -39,31 +40,54 @@ namespace FSAddonInstaller {
         static void Main(string[] args) {
             Console.WriteLine(WELCOME_MESSAGE);
 
-            // Get FSX directory
-            fsxDirectory = getFsxDirectory();
-            Console.WriteLine("FSX Directory:\t" + fsxDirectory + "\r\n");
-
             if (args.Length != 1) {
                 Console.WriteLine(INVALID_NUM_ARGS);
-            } else if (Directory.Exists(args[0])) {
-                state = State.INSTALL;
-                run(args[0]);
-            } else if (File.Exists(args[0]) && args[0].EndsWith(".xml")) {
-                state = State.UNINSTALL;
-                run(args[0]);
             } else {
-                Console.WriteLine(INVALID_DIR_OR_FILE);
+                try {
+                    Program p = new Program(args[0]);
+                    p.run();
+                } catch (ArgumentException e) {
+                    Console.WriteLine(e.Message);
+                }
             }
-
+            
             Console.WriteLine(EXIT_MESSAGE);
             Console.ReadKey();
         }
 
         /// <summary>
-        /// Runs the program
+        /// Default constructor
         /// </summary>
         /// <param name="path">The path to the directory or XML log file</param>
-        static void run(string path) {
+        Program(string path) {
+            // Argument checks
+            if (Directory.Exists(path)) {
+                state = State.INSTALL;
+            } else if (File.Exists(path) && path.EndsWith(".xml")) {
+                state = State.UNINSTALL;
+            } else {
+                throw new ArgumentException(INVALID_DIR_OR_FILE);
+            }
+
+            // Class member initialisation
+            path = null;
+            fsxDirectory = null;
+            directoryCount = 0;
+            fileCount = 0;
+            skipCount = 0;
+            backupCount = 0;
+            xmlConfig = null;
+            backupAndReplaceEnabled = true;
+
+            // Get FSX directory
+            fsxDirectory = getFsxDirectory();
+            Console.WriteLine("FSX Directory:\t" + fsxDirectory + "\r\n");
+        }
+
+        /// <summary>
+        /// Runs the program
+        /// </summary>
+        public void run() {
             switch (state) {
                 case State.INSTALL:
                     install(path);
@@ -80,7 +104,7 @@ namespace FSAddonInstaller {
         /// Installs the addon directory structure
         /// </summary>
         /// <param name="path">The directory to install</param>
-        static void install(string path) {
+        private void install(string path) {
             // Install setup
             xmlConfig = new XDocument(
                 new XDeclaration("1.0", "utf-8", "yes"),
@@ -105,7 +129,7 @@ namespace FSAddonInstaller {
         /// Uninstalls an addon using a given XML log
         /// </summary>
         /// <param name="path">The path to the XML file</param>
-        static void uninstall(string path) {
+        private void uninstall(string path) {
             XmlDocument input;
             XmlNodeList nodes;
 
@@ -146,7 +170,7 @@ namespace FSAddonInstaller {
         /// Gets the FSX root directory from the registry, including a backslash
         /// </summary>
         /// <returns>FSX root directory</returns>
-        static string getFsxDirectory() {
+        private string getFsxDirectory() {
             string dir = (string)Registry.GetValue(FSX_REG, "SetupPath", null);
 
             if (!string.IsNullOrEmpty(dir) && !dir.EndsWith("\\")) {
@@ -160,7 +184,7 @@ namespace FSAddonInstaller {
         /// Recursively loops through source directory to look for files and calls copying function
         /// </summary>
         /// <param name="source">Root directory to start copying at</param>
-        static void getFilesAndDirectories(string source) {
+        private void getFilesAndDirectories(string source) {
             foreach (string file in Directory.GetFiles(source)) {
                 // Get the relative path
                 string relPath = file.Substring(source.Length + 1, file.Length - (source.Length - 1));
@@ -182,7 +206,7 @@ namespace FSAddonInstaller {
         /// </summary>
         /// <param name="source">The full path to the source file</param>
         /// <param name="destination">The full path to the destination file</param>
-        static void copyFile(string source, string destination) {
+        private void copyFile(string source, string destination) {
             bool backupCreated = false;
 
             // Status output
